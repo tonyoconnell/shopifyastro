eff0808519fc8a731776ff45e5674044a3de277d
Anthony OConnell <tony@one.ie>
2023-03-18 01:42:42 +0700

    Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/edge";
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: netlify(),
   integrations: [tailwind({
     config: {
       applyBaseStyles: false
